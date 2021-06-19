78318a2dfaa3faf3783b113d744fe33e7cd63ac3
KadeejathilKubra <84235107+KadeejathilKubra@users.noreply.github.com>
2021-06-19 22:36:38 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,14 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int [], int);
+int max(int x[],int k)
+{
+	int t,i;
+	t=x[0];
+	for(i=1;i<k;i++)
+        {
+		if(x[i]>t)
+			t=x[i];
+	}
+	return(t);
+}
